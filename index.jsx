59457022b10647ab0ef2214729431bc706eb2806
edilsonFaculdade <edilsoncandeo@alunos.utfpr.edu.br>
2025-02-20 22:01:03 -0300

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,7 @@
+export default function home () {
+return (
+<view className="home>
+  <text calssname="title"> Welcome to PWA App </text>
+  <text classname="info">more information about PWA</text>
+  </view>
+):}
